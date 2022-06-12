aef365f2b0ece671b65da0e73959e7a1f2b51fa4
marionfr <74741185+marionfr@users.noreply.github.com>
2022-06-12 19:38:02 +0200

    comments

--- OpenCV_ISEP/Rezising.h
+++ OpenCV_ISEP/Rezising.h
@@ -13,15 +13,15 @@ double scaleY;
 
 Mat rezising(Mat src, double scaleX, double scaleY)
 {
+	//dispaly the dimension of the previous image
 	cout << "Current image height: " << src.size().height << endl;
 	cout << "Current image width : " << src.size().width << endl;
 
+	// the user chooses the multiplicating factor
 	cout << "Choose the multiplicator factor to modify the height of the image: ";
 	cin >> scaleX;
 	cout << "Choose the multiplicator factor to modify the width of the image: ";
 	cin >> scaleY;
-
-	//imshow("Image not modified", src);
 
 	// Scaling the image by dimension
 	resize(src, resized_src, Size(), scaleX, scaleY, INTER_LINEAR);
